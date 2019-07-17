2c9ad1b225bc9daa102e7a1e5c924c0a6c82b2df
jingwei <jingwei@ldomotors.com>
2019-07-17 17:19:58 +0800

    Add LPC177X_8XLib

--- CMSISv2p00_LPC17xx/inc/system_LPC177x_8x.h
+++ CMSISv2p00_LPC17xx/inc/system_LPC177x_8x.h
@@ -1,19 +1,15 @@
 /**********************************************************************
-* $Id$    system_LPC177x_8x.h      2011-06-02
+* $Id$		system_LPC177x_8x.h			2011-06-02
 *//**
-* @file    system_LPC177x_8x.h
-* @brief  CMSIS Cortex-M3 Device Peripheral Access Layer Source File
-*      for the NXP LPC177x_8x Device Series
-* @version  1.0
-* @date    02. June. 2011
-* @author  NXP MCU SW Application Team
-*
+* @file		system_LPC177x_8x.h
+* @brief	CMSIS Cortex-M3 Device Peripheral Access Layer Source File
+*			for the NXP LPC177x_8x Device Series
+* @version	1.0
+* @date		02. June. 2011
+* @author	NXP MCU SW Application Team
+* 
 * Copyright(C) 2011, NXP Semiconductor
 * All rights reserved.
-*
-* modified by KEIL:
-* 2016-12-07:
-* - moved clock macros from system_LPC177x_8x.h to system_LPC177x_8x.c
 *
 ***********************************************************************
 * Software that is described herein is for illustrative purposes only
@@ -26,6 +22,12 @@
 * notification. NXP Semiconductors also make no representation or
 * warranty that such application will be suitable for the specified
 * use without further testing or modification.
+* Permission to use, copy, modify, and distribute this software and its
+* documentation is hereby granted, under NXP Semiconductors'
+* relevant copyright in the software, without fee, provided that it
+* is used in conjunction with NXP Semiconductors microcontrollers.  This
+* copyright, permission, and disclaimer notice must appear in all copies of
+* this code.
 **********************************************************************/
 
 #ifndef __SYSTEM_LPC177x_8x_H
@@ -37,30 +39,57 @@ extern "C" {
 
 #include <stdint.h>
 
-extern uint32_t SystemCoreClock;      /*!< System Clock Frequency (Core Clock)    */
-extern uint32_t PeripheralClock;      /*!< Peripheral Clock Frequency (Pclk)      */
-extern uint32_t EMCClock;             /*!< EMC Clock                              */
-extern uint32_t USBClock;             /*!< USB Frequency                          */
+/** @addtogroup LPC177x_8x_System
+ * @{
+ */
+extern uint32_t SystemCoreClock;      /*!< System Clock Frequency (Core Clock)  	*/
+extern uint32_t PeripheralClock;	    /*!< Peripheral Clock Frequency (Pclk) 	    */
+extern uint32_t EMCClock;			        /*!< EMC Clock                              */
+extern uint32_t USBClock;			        /*!< USB Frequency 						              */
 
 
 /**
-  \brief  Setup the microcontroller system.
-
-    Initialize the System and update the SystemCoreClock variable.
+ * Initialize the system
+ *
+ * @param  none
+ * @return none
+ *
+ * @brief  Setup the microcontroller system.
+ *         Initialize the System and update the SystemCoreClock variable.
  */
 extern void SystemInit (void);
 
 /**
-  \brief  Update SystemCoreClock variable.
-
-    Updates the SystemCoreClock with current core Clock
-    retrieved from cpu registers.
+ * Update SystemCoreClock variable
+ *
+ * @param  none
+ * @return none
+ *
+ * @brief  Updates the SystemCoreClock with current core Clock
+ *         retrieved from cpu registers.
  */
 extern void SystemCoreClockUpdate (void);
 
+/*----------------------------------------------------------------------------
+  Define clocks
+ *----------------------------------------------------------------------------*/
+#define XTAL        (12000000UL)        /* Oscillator frequency               */
+#define OSC_CLK     (      XTAL)        /* Main oscillator frequency          */
+#define RTC_CLK     (   32768UL)        /* RTC oscillator frequency           */
+#define IRC_OSC     (12000000UL)        /* Internal RC oscillator frequency   */
+#define WDT_OSC		  (  500000UL)	    	/* Internal WDT oscillator frequency  */
+
+
+
+/*
+//-------- <<< end of configuration section >>> ------------------------------
+*/
 
 #ifdef __cplusplus
 }
 #endif
 
+/**
+ * @}
+ */
 #endif /* __SYSTEM_LPC177x_8x_H */
